3fb039db29114bf75d52cfa49505142ddcf381e2
Mithlesh Shah <mithleshshah84@gmail.com>
2024-06-26 20:33:09 +0530

    created controller for cart and product

---
+++ src/main/java/com/mith/keyclock/controller/CartController.java
@@ -0,0 +1,12 @@
+package com.mith.keyclock.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author mithlesh
+ * @date 25-06-2024
+ * @email mithleshshah84@gmail.com
+ */
+@RestController
+public class CartController {
+}
---
+++ src/main/java/com/mith/keyclock/controller/ProductController.java
@@ -0,0 +1,9 @@
+package com.mith.keyclock.controller;
+
+/**
+ * @author mithl
+ * @date 25-06-2024
+ * @email mithleshshah84@gmail.com
+ */
+public class ProductController {
+}
